ed63e4be9f16a948228e81b9afdd178735098e8e
Robert Spillane <robert.e.spillane@gmail.com>
2024-04-23 15:03:31 +0100

    drop down menu import from material ui

--- my-app/src/sections/components/Sort.tsx
+++ my-app/src/sections/components/Sort.tsx
@@ -1,4 +1,4 @@
-// import * as React from 'react';
+import * as React from 'react';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -19,16 +19,10 @@ const MenuProps = {
 };
 
 const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
+    'Detached',
+    'Semi-detached',
+    'Terrace',
+    'Flat'
 ];
 
 export default function MultipleSelectCheckmarks() {
